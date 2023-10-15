1835ef7198a13a521ce46d7cc681d7863b738514
andresalmada <andres.almadaa@gmail.com>
2023-10-15 20:09:39 -0300

    hice mas ejercicios

--- javascript.js
+++ javascript.js
@@ -43,3 +43,66 @@ Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, 
     - Documento: ${documento}.`
 
 alert (mensaje) */
+
+/* Lista de reproducción
+
+Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist y el título de tres canciones, y al finalizar se muestre un mensaje que diga, p. ej.: "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 'Final Countdown'". */
+
+/* let nombrePlaylist = prompt("ingrese el nombre de la playlist")
+let cancion1 = prompt("ingrese una cancion")
+let cancion2 = prompt("ingrese una segunda cancion")
+let cancion3 = prompt("ingrese una tercera cancion")
+
+
+let mensaje = `Se ha creado la playlist ${nombrePlaylist} con las canciones ${cancion1}, ${cancion2}, ${cancion3}.`
+
+alert (mensaje) */
+
+/* Dirección completa
+
+Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa, p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina". */
+
+/* let calle = prompt("ingrese su calle")
+let numeroCalle = prompt("ingrese el numero")
+let departamento = prompt("ingrese el departamento")
+let codigoPostal = prompt("ingrese el codigo postal")
+let ciudad = prompt("ingrese su ciudad")
+let pais = prompt("ingrese su pais")
+
+
+let mensaje = `La dirección que ha ingresado es: ${calle} ${numeroCalle} ${departamento}, ${codigoPostal}, ${ciudad}, ${pais}.`
+
+alert (mensaje) */
+
+/* Años perro
+
+Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro. */
+
+/* let edad = prompt ("ingrese una edad")
+let edadPerro = edad * 7
+
+alert (`La edad ${edad} en años perro es equivalente a ${edadPerro}.`) */
+
+/* Minutos a segundos
+
+Crear un programa que pida ingresar una cantidad en minutos mediante un y muestre un mensaje con el resultado de la conversión en segundos. */
+
+/* let ingresoMinutos = prompt ("ingrese una cantidad en minutos")
+let conversionASegundos = ingresoMinutos * 60 
+
+alert (`Los ${ingresoMinutos} minutos que ingresó, equivalen a ${conversionASegundos} segundos.`) */
+
+/* Días a segundos
+
+Crear un programa que pida ingresar una cantidad de días y muestre un mensaje con el resultado de la conversión en segundos.
+  */
+
+/* let ingresoDias = prompt ("ingrese una cantidad de dias")
+let conversionASegundos = ingresoDias * 86400 
+
+alert (`Teniendo en cuenta que cada dia tienen 86400 segundos.. Los ${ingresoDias} días que usted ingresó, equivalen a ${conversionASegundos} segundos.`) */
+
+/* Kilómetros a millas
+
+Crear un programa que pida ingresar una cantidad de kilómetros y muestre un mensaje con el resultado de la conversión en millas. */
+
