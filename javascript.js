0ef24622e5763786ce3b72de2bbfbb181e25eb36
andresalmada <andres.almadaa@gmail.com>
2023-10-14 21:19:14 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,45 @@
+/* Saludo
+
+Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada". */
+
+/* let nombre = prompt("ingrese nombre")
+let apellido = prompt("ingresu su apellido")
+
+let saludo = `Hola ${nombre} ${apellido}, bienvenido a Ada`;
+
+alert(saludo)  */
+
+
+
+/* Heladería
+
+Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada". */
+
+/* let gustoHelado1 = prompt("ingrese el primer gusto de helado")
+let gustoHelado2 = prompt("ingrese el segundo gusto de helado")
+let gustoHelado3 = prompt("ingrese el tercer gusto de helado")
+
+let mensaje = `Aquí tiene su helado de ${gustoHelado1}, ${gustoHelado2} y ${gustoHelado3}.`
+alert (mensaje) */
+
+
+
+/* Datos personales
+
+Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos. */
+
+
+/*     let nombre = prompt("ingrese su nombre")
+    let apellido = prompt("ingrese su apellido")
+    let edad = prompt("ingrese su edad")
+    let nacionalidad = prompt("ingrese su nacionalidad")
+    let documento = prompt("ingrese su documento")
+
+    let mensaje = `Nuevo usuario agregado al sistema:
+    - Nombre: ${nombre}
+    - Apellido: ${apellido}
+    - Edad: ${edad}
+    - Nacionalidad: ${nacionalidad}
+    - Documento: ${documento}.`
+
+alert (mensaje) */
